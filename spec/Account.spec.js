4654f32b2b12360be83f4a6fcb8b1cb08d818434
Tilly Mallabar <tilly.m94@gmail.com>
2024-05-03 14:29:18 +0100

    TEST PASSED, initial balance is 0

--- spec/Account.spec.js
+++ spec/Account.spec.js
@@ -3,6 +3,11 @@ import Account from "../src/Account.js";
 describe("Account Tests: ", () => {
     let testAccount, testName;
 
+    beforeEach(() => {
+        testName = "Test Name";
+        testAccount;
+    });
+
     it("should be instance of Account", () => {
         // Arrange
         testAccount = new Account();
@@ -26,6 +31,14 @@ describe("Account Tests: ", () => {
         // Act
         // Assert
         expect(() => new Account(testName)).toThrowError(Error)
+    });
+    
+    it("should have an initial balance of 0", () => {
+        // Arrange
+        testAccount = new Account(testName);
+        // Act
+        // Assert
+        expect(testAccount.getBalance()).toBe(0);
     });
 
 });
--- src/Account.js
+++ src/Account.js
@@ -2,6 +2,11 @@ export default class Account {
     constructor(name) {
         if (name === null) throw new Error("Name cannot be null");
         this.name = name;
-        
+    }
+
+    balance = 0;
+
+    getBalance() {
+        return this.balance;
     }
  };
